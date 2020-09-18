0b2773c4257f0b86e55137f59642b91c96e58049
Aniket <agade@osisoft.com>
2020-09-18 14:53:11 -0700

    function predict assignment

---
+++ Web_Fundamentals/Javascript/function_predict/predict_3.js
@@ -0,0 +1,15 @@
+function highFive(num){
+    for(var i=0; i<num; i++){
+        if(i == 5){
+            console.log("High Five!");
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
+
+
+/*
+Output - No output because the function is never called
+*/
