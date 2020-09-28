0d7537cbc2d98ddf215ac10e180d566b0866f6cd
Aniket <agade@osisoft.com>
2020-09-28 16:51:03 -0700

    color clicker assignment

--- Web_Fundamentals/JQuery/ninja_to_cat/script.js
+++ Web_Fundamentals/JQuery/ninja_to_cat/script.js
@@ -1,16 +1,40 @@
 $(document).ready(function(){
 
     console.log("Ready!");
+    
+    var clicked = false;
+
+    // how to optimize the below code which is repeating, if I put this block in a function , then it doesn't work
+    // also, how to disable hover out when the img is clicked, to improve visual experience
 
     $('img').click(function(){
 
-        console.log($(this).attr('otherImg'));
+        clicked = true;
 
-        var other_img = $(this).attr('otherImg');
-        var img_src = $(this).attr('src');
+        let other_img = $(this).attr('otherImg');
+        let img_src = $(this).attr('src');
+
         $(this).attr('src',other_img);
         $(this).attr('otherImg',img_src);
-
     });
 
+    $('img').hover(function(){
+        let other_img = $(this).attr('otherImg');
+        let img_src = $(this).attr('src');
+
+        $(this).attr('src',other_img);
+        $(this).attr('otherImg',img_src);
+    }, 
+    function(){
+        
+        let other_img = $(this).attr('otherImg');
+        let img_src = $(this).attr('src');
+
+        $(this).attr('src',other_img);
+        $(this).attr('otherImg',img_src);
+    })
+
+
  });
+
+ 
