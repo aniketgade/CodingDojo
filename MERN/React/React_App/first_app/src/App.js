d4d3e7a474819129fd4ee4998842bab265c90991
Aniket <agade@osisoft.com>
2020-10-07 14:57:22 -0700

    First JSX assignment

---
+++ MERN/React/React_App/first_app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Clim Mt. Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
